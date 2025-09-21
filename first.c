dda05add0842f1fc57ee3edf4e2c8ec800167c80
Harsh <kaoruko201808@gmail.com>
2025-09-21 23:13:42 +0530

    Add files via upload

---
+++ first.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <math.h> 
+int main(){
+int r, area;
+printf("enter radius: ");
+scanf("%d",&r);
+area= 3.14*(pow(2,r));
+printf("Area of circle :  %d", area);
+return 0;
+
+}
