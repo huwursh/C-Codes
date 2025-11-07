33d677d5e67d15c22e1a36572c9067f5876dbe2e
Harsh <kaoruko201808@gmail.com>
2025-11-07 23:15:23 +0530

    Add files via upload

---
+++ second.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <math.h>
+int main()
+{
+    int a = 2;
+    int b= a+1;
+    int x,y,z;
+    x=y=z=1;
+    int J = (int)69.9989898;
+    printf("%d\n",J);
+    int sum = a+b, multiphy=a*b;
+    int power =pow(a,b);      // A to the power B pow(A,B)
+    printf("power of a to power b equals %d\n",power);          
+    printf("%d\n",-10%3);    //Modulus function used for remainder
+    printf("%f\n",2.2*11);
+    printf("%f\n",3.0/2);
+
+    return 0;
+    
+}
